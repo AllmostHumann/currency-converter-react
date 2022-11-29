e6f4d6413f93042c739d545573f3013793a17cfd
Jakub Gluch <jakub.gluch92@gmail.com>
2022-11-29 22:10:28 +0100

    folder name changer, added id for components

--- src/Currency/index.js
+++ src/Currency/index.js
@@ -1,17 +1,20 @@
 export const currencies = [
     {
+        id: 1,
         short: "EUR",
         name: "Euro",
         rate: 4.7723
     },
 
     {
+        id: 2,
         short: "USD",
         name: "Dolar Amerykański",
         rate: 4.7538
     },
-    
+
     {
+        id: 3,
         short: "NOK",
         name: "Korona Norweska",
         rate: 0.4604
