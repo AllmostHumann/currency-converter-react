877b5c78a9062d6787caaac70e1c725fdd928f37
Jakub Gluch <jakub.gluch92@gmail.com>
2022-11-29 19:57:30 +0100

    rebuild of all contenct

--- src/App.js
+++ src/App.js
@@ -1,9 +1,4 @@
 import Header from "./Header";
-import Amount from "./Amount";
-import Buttons from "./Buttons";
-import Currency from "./Currency";
-import Result from "./Result";
-import Footer from "./Footer";
 import Form from "./Form";
 import Container from "./Container";
 
@@ -13,13 +8,7 @@ function App() {
     <Container>
       <Header
         title="Czinkciarz.peel" />
-      <Form>
-        <Amount />
-        <Currency />
-        <Result />
-        <Buttons />
-        <Footer />
-      </Form>
+      <Form />
     </Container>
   );
 }
--- src/Form/index.js
+++ src/Form/index.js
@@ -1,14 +1,48 @@
 import "./style.css";
 
-const Form = ({ children }) => (
-    <fieldset className="form__fieldset">
-        <legend
-            className="form__legend">
-            Currency conventer
-        </legend>
-        {children}
-    </fieldset>
-    
+const Form = () => (
+  <fieldset className="form__fieldset">
+    <legend
+      className="form__legend">
+      Currency conventer
+    </legend>
+    <section className="form__label">
+      <label>
+        Wpisz kwotę w PLN:
+        <input
+          className="form__amountValue"
+          type="number"
+          min="0.01"
+          required step="0.01"
+          placeholder="Pole wymagane" />
+      </label>
+    </section>
+    <section className="form__label">
+      <label>
+        Wybierz walutę:
+        <select className="form__currencyValue" name="currency" id="select">
+          <option value="EUR">Euro (EUR)</option>
+          <option value="USD">Dolar amerykański (USD)</option>
+          <option value="NOK">Korona norweska (NOK)</option>
+        </select>
+      </label>
+    </section>
+    <section className="form__label">
+      <label>
+        Otrzymasz:
+        <strong className="form__resultValue">N/A</strong>
+      </label>
+    </section>
+    <div>
+      <button className="form__Button" type="submit">Przelicz</button>
+      <button className="form__Button" type="reset">Reset</button>
+    </div>
+    <section>
+      <p>*Kursy walut pobrane z Narodowego Banku Polskiego.<br />
+        Aktualne na dzień: <strong>2022-10-26</strong></p>
+    </section>
+  </fieldset>
+
 );
 
 export default Form;
