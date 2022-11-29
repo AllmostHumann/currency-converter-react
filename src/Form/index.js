677eefc34586a7be1469395fe35f42d00bb00d9f
Jakub Gluch <jakub.gluch92@gmail.com>
2022-11-29 22:11:22 +0100

    added functionality

--- src/Form/index.js
+++ src/Form/index.js
@@ -1,80 +1,104 @@
 import "./style.css";
+import { currencies } from "../Currency/index";
+import { useState } from "react";
 
-const Form = () => (
-  <fieldset
-    className="form__fieldset">
-    <legend
-      className="form__legend"
+const Form = ({ result, calculatedResult, getResult }) => {
+
+  const [exchangeAmount, setExchangeAmount] = useState("");
+  const [currency, setCurrency] = useState("EUR");
+
+  const onFormSubmit = (event) => {
+    event.preventDefault();
+    calculatedResult(exchangeAmount, currency);
+  };
+
+  const onFormReset = () => {
+    setExchangeAmount("");
+    getResult("");
+  };
+
+  return (
+    <form
+      onSubmit={onFormSubmit}
+      onReset={onFormReset}
     >
-      Currency conventer
-    </legend>
-    <section
-      className="form__label"
-    >
-      <label>
-        Wpisz kwotę w PLN:
-        <input
-          className="form__amountValue"
-          type="number"
-          min="0.01"
-          required step="0.01"
-          placeholder="Pole wymagane"
-        />
-      </label>
-    </section>
-    <section
-      className="form__label">
-      <label>
-        Wybierz walutę:
-        <select
-          className="form__currencyValue" name="currency" id="select">
-          <option
-            value="EUR"
-          >
-            Euro (EUR)
-          </option>
-          <option
-            value="USD"
-          >Dolar amerykański (USD)
-          </option>
-          <option
-            value="NOK"
-          >Korona norweska (NOK)
-          </option>
-        </select>
-      </label>
-    </section>
-    <section
-      className="form__label">
-      <label>
-        Otrzymasz:
-        <strong
-          className="form__resultValue"
+      <fieldset
+        className="form__fieldset"
+      >
+        <legend
+          className="form__legend"
         >
-          N/A
-        </strong>
-      </label>
-    </section>
-    <div>
-      <button
-        className="form__Button"
-        type="submit"
-      >
-        Przelicz
-      </button>
-      <button
-        className="form__Button"
-        type="reset"
-      >
-        Reset
-      </button>
-    </div>
-    <section>
-      <p>*Kursy walut pobrane z Narodowego Banku Polskiego.<br />
-        Aktualne na dzień: <strong>2022-10-26</strong></p>
-    </section>
-  </fieldset>
-
-);
+          Currency conventer
+        </legend>
+        <section
+          className="form__label"
+        >
+          <label>
+            Wpisz kwotę w PLN:
+            <input
+              className="form__amountValue"
+              type="number"
+              min="0.01"
+              required step="0.01"
+              placeholder="Pole wymagane"
+              value={exchangeAmount}
+              onChange={({ target }) => setExchangeAmount(target.value)}
+            />
+          </label>
+        </section>
+        <section
+          className="form__label">
+          <label>
+            Wybierz walutę:
+            <select
+              className="form__currencyValue"
+              name="currency"
+              value={currency}
+              onChange={({ target }) => setCurrency(target.value)}
+            >
+              {currencies.map(currency => (
+                <option
+                  key={currency.id}
+                  value={currency.short}
+                >
+                  {currency.name}
+                </option>
+              ))};
+            </select>
+          </label>
+        </section>
+        <section
+          className="form__label">
+          <label>
+            Otrzymasz:
+            <strong
+              className="form__resultValue"
+            >
+              {result}
+            </strong>
+          </label>
+        </section>
+        <div>
+          <button
+            className="form__Button"
+            type="submit"
+          >
+            Przelicz
+          </button>
+          <button
+            className="form__Button"
+            type="reset"
+          >
+            Reset
+          </button>
+        </div>
+        <section>
+          <p>*Kursy walut pobrane z Narodowego Banku Polskiego.<br />
+            Aktualne na dzień: <strong>2022-10-26</strong></p>
+        </section>
+      </fieldset>
+    </form>
+  );
+}
 
 export default Form;
