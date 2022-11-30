390e48f550a5708d344de84e8eda355ac27c8a87
Jakub Gluch <jakub.gluch92@gmail.com>
2022-11-30 19:33:54 +0100

    formules moved from app to form

--- src/App/App.js
+++ src/App/App.js
@@ -1,30 +1,14 @@
 import Header from "../Header";
 import Form from "../Form";
 import Container from "../Container";
-import { currencies } from "../Currency/index";
-import { useState } from "react";
 
 function App() {
-
-  const [result, getResult] = useState();
-
-  const calculatedResult = (exchangeAmount, currency) => {
-    const exchangeRate = currencies.find(({ short }) => short === currency).rate;
-    const short = currencies.find(({ short }) => short === currency).short;
-
-    getResult((exchangeAmount / exchangeRate).toFixed(2) + " " + short);
-
-  };
 
   return (
     <Container>
       <Header
         title="Czinkciarz.peel" />
-      <Form
-        calculatedResult={calculatedResult}
-        getResult={getResult}
-        result={result}
-      />
+      <Form />
     </Container>
   );
 }
--- src/Form/index.js
+++ src/Form/index.js
@@ -1,5 +1,5 @@
 import "./style.css";
-import { useState } from "react";
+import React, { useState } from "react";
 import { currencies } from "../Currencies/index";
 import Footer from "../Footer";
 import Buttons from "../Buttons";
@@ -8,7 +8,7 @@ import Select from "../Select";
 import Input from "../Input";
 import Legend from "../Legend";
 
-const Form = ({ result, calculatedResult, getResult }) => {
+const Form = () => {
 
   const [exchangeAmount, setExchangeAmount] = useState("");
   const [currency, setCurrency] = useState("EUR");
@@ -23,32 +23,47 @@ const Form = ({ result, calculatedResult, getResult }) => {
     getResult("");
   };
 
+  const [result, getResult] = useState();
+
+  const calculatedResult = (exchangeAmount, currency) => {
+    const exchangeRate = currencies.find(({ short }) => short === currency).rate;
+    const short = currencies.find(({ short }) => short === currency).short;
+
+    getResult((exchangeAmount / exchangeRate).toFixed(2) + " " + short);
+
+  };
+
   return (
-    <form
-      onSubmit={onFormSubmit}
-      onReset={onFormReset}
-    >
-      <fieldset
-        className="form__fieldset"
+    
+      <form
+        onSubmit={onFormSubmit}
+        onReset={onFormReset}
+        result={result}
       >
-        <Legend />
-        <Input
-          exchangeAmount={exchangeAmount}
-          setExchangeAmount={setExchangeAmount}
-        />
-        <Select
-          currency={currency}
-          setCurrency={setCurrency}
-          currencies={currencies}
-        />
-        <Result
-          result={result}
-        />
-        <Buttons />
-        <Footer />
-      </fieldset>
-    </form>
+        <fieldset
+          className="form__fieldset"
+        >
+          <Legend />
+          <Input
+            exchangeAmount={exchangeAmount}
+            setExchangeAmount={setExchangeAmount}
+          />
+          <Select
+            currency={currency}
+            setCurrency={setCurrency}
+            currencies={currencies}
+          />
+          <Result
+            result={result}
+          />
+          <Buttons />
+          <Footer />
+        </fieldset>
+      </form>
+    
   );
-}
+
+};
+
 
 export default Form;
