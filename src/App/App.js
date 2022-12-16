c0d29c171851b9a01dc647d9acf585648cc5ce04
Jakub Gluch <jakub.gluch92@gmail.com>
2022-12-16 12:11:26 +0100

    removed container component

--- src/App/App.js
+++ src/App/App.js
@@ -1,17 +1,16 @@
 import Header from "../Header";
 import Form from "../Form";
-import Container from "../Container";
 import Footer from "../Footer";
 
 function App() {
 
   return (
-    <Container>
+    <>
       <Header
         title="Czinkciarz.peel" />
       <Form />
       <Footer />
-    </Container>
+    </>
   );
 }
 
