326cf558447fec3b2821e4c0ae8231781ae0b760
Jakub Gluch <jakub.gluch92@gmail.com>
2023-01-12 10:25:19 +0100

    added indentation for Footer

--- src/App/Footer/index.js
+++ src/App/Footer/index.js
@@ -1,10 +1,10 @@
 import styled from "styled-components";
 
 const Footer = styled.footer`
-    text-align: center;
-    margin: 10px 15px;
-    font-weight: 700;
-    color: rgb(255, 165, 0)
+  text-align: center;
+  margin: 10px 15px;
+  font-weight: 700;
+  color: rgb(255, 165, 0)
 `;
 
 export default Footer;
