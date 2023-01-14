ea08be2fb8360858601018f67a513d5fd195237a
Jakub Gluch <jakub.gluch92@gmail.com>
2023-01-14 14:59:28 +0100

    created currenciesAPI.js

---
+++ src/App/Form/Common/currenciesAPI.js
@@ -0,0 +1,29 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
+
+export const useRatesData = () => {
+    const [ratesData, setRatesData] = useState({
+        status: "wczytywanie",
+    });
+
+
+useEffect(() => {
+    const getRates = async () => {
+        try {
+            const response = await axios.get("https://api.exchangerate.host/latest?=basePLN");
+            setRatesData({
+                date: response.data.date,
+                rates: response.data.rates,
+                status: "sukces",
+            });
+        } catch (error) {
+            setRatesData({
+                status: "Coś się popsuło",
+            });
+        }
+    };
+    setTimeout(getRates, 3000);
+},[]);
+
+return ratesData;
+};
