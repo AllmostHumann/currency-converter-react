49ae92825246bdd574e01d563bd194e7fa386439
Jakub Gluch <jakub.gluch92@gmail.com>
2023-01-15 16:16:45 +0100

    Info compontent upd for every refresh of EBC API

--- src/App/Form/Info/index.js
+++ src/App/Form/Info/index.js
@@ -1,7 +1,7 @@
-const Info = () => (
+const Info = ({ date }) => (
     <section>
-        <p>*Kursy walut pobrane z Narodowego Banku Polskiego.<br />
-            Aktualne na dzień: <strong>2022-10-26</strong></p>
+        <p>*Kursy walut pobrane z Europejskiego Banku Centralnego.<br />
+            Aktualne na dzień: <strong>{date}</strong></p>
     </section>
 );
 
